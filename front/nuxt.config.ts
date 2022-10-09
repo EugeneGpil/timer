0b00dd3a42c69082c9ec99e6bda53ec7648bd0d1
EugeneGpil <gpilligrim@gmail.com>
2022-10-09 06:52:08 +0300

    1

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  css: [
+    'normalize.css',
+    '@/assets/css/main.scss',
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/css/colors.scss";',
+        },
+      },
+    },
+  },
 })
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,3 +1,37 @@
 <template>
-  HI
+  <div>
+    <section class="main-block">
+
+    </section>
+
+    <section class="footer">
+      <img class="footer__logo" src="/img/github.png" alt="github logo">
+    </section>
+
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.wrapper {
+  height: 100%;
+}
+
+.main-block {
+  height: 100vh;
+  background-color: $background-color;
+}
+
+.footer {
+  height: 74px;
+  background-color: $secondary-color;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding-right: 24px;
+
+  &__logo {
+    width: 42px;
+    height: 42px;
+  }
+}
+</style>
